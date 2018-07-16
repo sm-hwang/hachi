143fb57b9e218d4e2cb10ef215c69d3a7f258d47
sm-hwang <39686547+sm-hwang@users.noreply.github.com>
2018-07-15 21:50:28 -0600

    Update synthesis.py

--- synthesis.py
+++ synthesis.py
@@ -1,25 +1,13 @@
-
-
 import argparse
 import logging
 import random
 import sys
-
-#Can be optimzed using numpy
-#Check if the specified oligo_size does not match the oligo in the input file
-#Add progress bars using tqdm
-#Substitution can't be the same base it started out with
+import numpy as np
 
 MUTATION_CHANCE = 1/500
 HEADER = 'packet'
 DELETION = 0
 INSERTION = 1
-A = 0 
-C = 1
-G = 2
-T = 3
-P = 4
-Z = 5
 
 def read_args():
     parser = argparse.ArgumentParser()
@@ -33,37 +21,15 @@ def read_args():
 
     
 def mutate(base):
-    chance = random.randrange(0,3)
+    chance = np.random.randint(0,3)
+    nt_list = ['A','C','G','T','P','Z']
     if chance == DELETION:
         nt = ""
     elif chance == INSERTION:
-        insert_nt = random.randrange(0,6)
-        if insert_nt == A:
-            nt = base + 'A'
-        elif insert_nt == C:
-            nt = base + 'C'
-        elif insert_nt == G:
-            nt = base + 'G'
-        elif insert_nt == T:
-            nt = base + 'T'
-        elif insert_nt == P:
-            nt = base + 'P'
-        else:
-            nt = base + 'Z'
+        nt = base + np.random.choice(nt_list)
     else: #Substitution
-        sub_nt = random.randrange(0,6)
-        if sub_nt == A:
-            nt = 'A'
-        elif sub_nt == C:
-            nt = 'C'
-        elif sub_nt == G:
-            nt = 'G'
-        elif sub_nt == T:
-            nt = 'T'
-        elif sub_nt == P:
-            nt = 'P'
-        else:
-            nt = 'Z'
+        nt_list.remove(base)
+        nt = np.random.choice(nt_list)
     return nt
 
 
@@ -86,12 +52,14 @@ def main():
         
         if HEADER in oligo: #Reading the header for the oligo
             continue
-
-        mutated = [random.randrange(0, int(MUTATION_CHANCE ** -1)) for i in range(arg.oligo_size)] #generating list of random integers between 0 - (1/MUTATION_CHANCE). List size == oligo size in nt.
+        
+        oligo.rstrip("\n")
+        
+        mutated = np.random.randint(int(MUTATION_CHANCE ** -1), size= len(oligo)) #generating list of random integers between 0 - (1/MUTATION_CHANCE). Length of list is equal to oligo size in nt.
 
         synth_dna = []
 
-        for i, chance in enumerate(mutated):
+        for i, chance in enumerate(mutated):            #np.ndenumerate() for numpy
             if chance == 0:                             #If mutated[i] == 0, then it is mutated 
                 synth_dna.append(mutate(oligo[i]))
             else:
@@ -100,6 +68,6 @@ def main():
         out.write("".join(synth_dna))
         
     f.close()
-        
-        
+    out.close()
+          
 main()
