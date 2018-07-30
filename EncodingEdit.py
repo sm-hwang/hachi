76f78bcc578b045374b2f9e978fcb25de0da98b6
sm-hwang <39686547+sm-hwang@users.noreply.github.com>
2018-07-29 21:45:46 -0600

    Update EncodingEdit.py

--- EncodingEdit.py
+++ EncodingEdit.py
@@ -1,7 +1,7 @@
 """
 Modified from https://github.com/TeamErlich/dna-fountain
+Replace the methods in util.pyx with the ones provided below. 
 """
-
 
 #6 Nucleotide
 
@@ -35,6 +35,20 @@ def prepare(max_repeat):
     MAX_Gs = '2' * (max_repeat)
     ADD_Z = '1' * (max_repeat - 1) + '5'
     ADD_P = '2' * (max_repeat - 1) + '4'
+    
+#Conversion happens from here
+six_intab = "012321"
+
+six_revtab = string.maketrans(outtab, six_intab)
+
+def dna_to_int_array(dna_str):
+    #convert a string like ACTCA to an array of ints like [10, 2, 4]
+    num = dna_str.translate(six_revtab)
+    s = ''.join('{0:02b}'.format(int(num[t])) for t in xrange(0, len(num),1))
+    print s
+    data = [int(s[t:t+8],2) for t in xrange(0,len(s), 8)]
+
+    return data
 
 #8 Nucleotide Assumption
 
@@ -64,10 +78,19 @@ def screen_repeat_dna(dna, max_repeat, gc_dev):
 
     if As in dna or Cs in dna or Gs in dna or Ts in dna or Ps in dna or Zs in dna or Xs in dna or Ys in dna: 
         return 0
-    
+    #Possibly add a filter so every nucleotide composes approximately 1/8th of the oligo 
     #gc = dna.count("1") + dna.count("2")  
     #gc = gc/(len(dna)+0.0)
 
     #if (gc < 0.5 - gc_dev) or (gc > 0.5 + gc_dev):
         #return 0
     return 1
+
+def dna_to_int_array(dna_str):
+    #convert a string like ACTCA to an array of ints like [10, 2, 4]
+    num = dna_str.translate(revtab)
+    s = ''.join('{0:03b}'.format(int(num[t])) for t in xrange(0, len(num),1))
+    print s
+    data = [int(s[t:t+8],2) for t in xrange(0,len(s), 8)]
+
+    return data
