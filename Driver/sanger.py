d56171f6f5612b519a5b81b91215b01a874080b9
sm-hwang <39686547+sm-hwang@users.noreply.github.com>
2018-08-05 20:35:21 -0600

    Update sanger.py

--- Driver/sanger.py
+++ Driver/sanger.py
@@ -1,19 +1,21 @@
-import sys
 import numpy as np
 from tqdm import tqdm
 
 def choose_oligos(pool_size, stop):
+    #returns np.array of size "stop" of numbers between and pool_size - 1
+    #Every number in the array is unique
     return np.random.choice(a= pool_size, size= stop, replace= False)
 
 def sequence(np_pool, stop):
     pbar = tqdm(total= stop, desc= "Sequenced oligos")
     
-    sequence_data = []
+    sequence_data = [None] * stop
     picked = choose_oligos(np_pool.size, stop)
     for seq in picked:
-        sequence_data.append(np_pool[seq])
+        sequence_data[seq] = np_pool[seq]
         pbar.update()
     pbar.close()
+    
     return np.array(sequence_data)
     
 def calc_stop(num, per, size):
